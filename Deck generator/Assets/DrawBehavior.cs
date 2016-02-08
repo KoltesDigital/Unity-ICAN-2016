a63de8e939efa680deb90d7b38e97323a8c9442e
Jonathan Giroux <bloutiouf@gmail.com>
2016-02-08 19:14:54 +0100

    Add 'Deck generator' example

---
+++ Deck generator/Assets/DrawBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DrawBehavior : MonoBehaviour
+{
+    public int CardCount = 10;
+
+    private DeckGenerator<int> generator = new DeckGenerator<int>();
+    
+    void Start()
+    {
+        // Create the cards once
+        for (int i = 0; i < CardCount; ++i)
+            generator.Add(i);
+    }
+
+	void Update()
+    {
+	    if (Input.anyKeyDown)
+        {
+            int card = generator.Draw();
+            Debug.Log(card);
+        }
+	}
+}
---
+++ Deck generator/Readme.md
@@ -0,0 +1,8 @@
+# Deck generator
+
+This example shows how to generate random elements based on a deck and a discard pile (card game analogy).
+
+## Take away
+
+1. [Classes](https://msdn.microsoft.com/en-us/library/x9afc042.aspx)
+2. [Generics](https://msdn.microsoft.com/en-us/library/512aeb7t.aspx)
