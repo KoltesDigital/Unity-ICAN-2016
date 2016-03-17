59378345dffec1745d422621382ffdf152b511e5
Jonathan Giroux <bloutiouf@gmail.com>
2016-03-17 14:07:49 +0100

    Add 'Spring' project

---
+++ Spring/Assets/Spring.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour
+{
+    public float mass = 1f;
+    public float stiffness = 1f;
+    public float damping = 0.5f;
+    
+    public Vector3 speed = Vector3.zero;
+    
+    void Update()
+    {
+        // Spring force
+        Vector3 position = transform.localPosition;
+        Vector3 forces = - position * stiffness;
+        
+        // Newton's second law
+        // sum of forces = mass * acceleration
+        Vector3 acceleration = forces / mass;
+
+        // Euler method for speed integration 
+        speed += acceleration * Time.deltaTime;
+
+        // Damping
+        speed *= 1.0f / (1.0f + Time.deltaTime * damping);
+
+        // Position integration
+        transform.localPosition += speed * Time.deltaTime;
+    }
+}
---
+++ Spring/Readme.md
@@ -0,0 +1,14 @@
+# Spring
+
+This example simulates a spring.
+
+## Takeaways
+
+1. [Newton's laws of motion](https://en.wikipedia.org/wiki/Newton%27s_laws_of_motion)
+2. [Hooke's law](https://en.wikipedia.org/wiki/Hooke%27s_law)
+3. [Euler method](https://en.wikipedia.org/wiki/Euler_method)
+4. [Damping](https://en.wikipedia.org/wiki/Damping)
+
+## Further reading
+
+* [Integration methods with example](http://codeflow.org/entries/2010/aug/28/integration-by-example-euler-vs-verlet-vs-runge-kutta/)
