a5f59e17de8fd0910e03a9acffdd6d1389fe1cf9
Jonathan Giroux <bloutiouf@gmail.com>
2016-03-17 13:54:50 +0100

    Add 'Smooth camera' project

---
+++ Smooth camera/Assets/SmoothCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour
+{
+    public float lerpRatio = 5.0f;
+    public float moveFactor = 0.1f;
+
+    private Vector3 target;
+    
+    void Start ()
+    {
+        target = transform.position;
+	}
+	
+    void Update ()
+    {
+        target.x += Input.GetAxis("Mouse X") * moveFactor;
+        target.y += Input.GetAxis("Mouse Y") * moveFactor;
+
+        transform.position = Vector3.Lerp(transform.position, target, lerpRatio * Time.deltaTime);
+    }
+}
---
+++ Smooth camera/Readme.md
@@ -0,0 +1,11 @@
+# Smooth camera
+
+This example moves the camera smoothly.
+
+Move the mouse in order to move the camera target.
+
+## Takeaways
+
+1. [Lerp](https://en.wikipedia.org/wiki/Linear_interpolation)
+2. [Mathf.Lerp](http://docs.unity3d.com/ScriptReference/Mathf.Lerp.html)
+3. [Vector3.Lerp](http://docs.unity3d.com/ScriptReference/Vector3.Lerp.html)
