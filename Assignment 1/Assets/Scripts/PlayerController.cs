78bcde37fa5013f59ea22e7e8eaa1ebcd13288db
Jonathan Giroux <bloutiouf@gmail.com>
2016-03-20 12:24:24 +0100

    Car strafing

---
+++ Assignment 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RoadFollower))]
+public class PlayerController : MonoBehaviour {
+    public float cylinderAngle = -0.6f;
+    public float maxTurnAngle = 0.5f;
+    public float strafeRatio = 5.0f;
+
+    RoadFollower roadFollower;
+
+    void Awake()
+    {
+        roadFollower = GetComponent<RoadFollower>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        roadFollower.SetCylinderAngle(cylinderAngle);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        roadFollower.SetRotationAngle(horizontal * maxTurnAngle);
+        roadFollower.AdvanceX(horizontal * strafeRatio);
+    }
+}
--- Assignment 1/Assets/Scripts/RoadFollower.cs
+++ Assignment 1/Assets/Scripts/RoadFollower.cs
@@ -3,21 +3,69 @@ using System.Collections;
 
 public class RoadFollower : MonoBehaviour
 {
-    public float angle = 0.0f;
+    // angle around the cylinder
+    // this drives the object's YZ coordinates
+    float cylinderAngle = 0.0f;
+
+    // object rotation around its Z axis to simulate the car turning
+    float rotationTargetAngle = 0.0f;
+    float rotationCurrentAngle = 0.0f;
+
+    // cache variable to assign to transform.localPosition
+    Vector3 position;
 
     const float RoadRadius = 50.0f;
     const float EdgeAngle = 1.08f;
+    public float LerpRatio = 1.0f;
 
-	void Update()
+    void Start()
     {
-        Vector3 position = transform.localPosition;
+        position = transform.localPosition;
+    }
 
-        // snap object to the ground
-        position.y = (Mathf.Cos(angle) - 1.0f) * RoadRadius;
-        position.z = Mathf.Sin(angle) * RoadRadius;
-        transform.localPosition = position;
+    void Update()
+    {
+        rotationCurrentAngle = Mathf.Lerp(rotationCurrentAngle, rotationTargetAngle, LerpRatio * Time.deltaTime);
 
         // orient object to the normal
-        transform.localRotation = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.right);
+        Quaternion rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * cylinderAngle, Vector3.right) * Quaternion.AngleAxis(Mathf.Rad2Deg * rotationCurrentAngle, Vector3.up);
+        transform.localRotation = rotation;
+    }
+
+    void UpdateYZPosition()
+    {
+        // snap object to the ground
+        position.y = (Mathf.Cos(cylinderAngle) - 1.0f) * RoadRadius;
+        position.z = Mathf.Sin(cylinderAngle) * RoadRadius;
+        transform.localPosition = position;
+    }
+
+    public void SetX(float x)
+    {
+        position.x = x;
+        transform.localPosition = position;
+    }
+
+    public void AdvanceX(float dx)
+    {
+        position.x += dx;
+        transform.localPosition = position;
+    }
+
+    public void SetCylinderAngle(float angle)
+    {
+        cylinderAngle = angle;
+        UpdateYZPosition();
+    }
+
+    public void AdvanceCylinderAngle(float da)
+    {
+        cylinderAngle += da;
+        UpdateYZPosition();
+    }
+
+    public void SetRotationAngle(float angle)
+    {
+        rotationTargetAngle = angle;
     }
 }
