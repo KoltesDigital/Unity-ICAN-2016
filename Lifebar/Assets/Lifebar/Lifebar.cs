2317b945fc98e13d07b9408fdedb8563a1ddaac9
Jonathan Giroux <bloutiouf@gmail.com>
2016-04-16 12:25:12 +0200

    Add Lifebar project

---
+++ Lifebar/Assets/Lifebar/Lifebar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Lifebar : MonoBehaviour
+{
+    // Although these properties are public, do not access them directly!
+    // They are only public because of the Inspector.
+    public Image avatarImage;
+    public Image lifeImage;
+    public Image manaImage;
+    public Image[] flagImages;
+}
---
+++ Lifebar/Readme.md
@@ -0,0 +1,9 @@
+# Lifebar
+
+This example displays a life bar which can be controled.
+
+## Take aways
+
+1. UI
+2. [Black box](https://en.wikipedia.org/wiki/Black_box)
+3. Shaders
